b0ac34c7f8c3d1cfd847314c256d7323d3a2f987
HyperK3122 <92637286+HyperK3122@users.noreply.github.com>
2023-05-14 20:21:01 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 from moviepy.editor import AudioFileClip, ImageClip
+print("Don't set FPS to 1 otherwise audio won't work")
 
 def mp3PNGMerge(fileSaveName):
     audio_clip = AudioFileClip(input("Path to Audio? ").strip('\"'))
     image_clip = ImageClip(input("Path to Image? ").strip('\"'))
     video_clip = image_clip.set_audio(audio_clip)
     video_clip.duration = audio_clip.duration
-    video_clip.fps = 30
+    fp = int(Input("Frames per second? (Integer only)"))
+    video_clip.fps = fp
     video_clip.write_videofile(fileSaveName + '.mp4')
 
 name = input("Name? ")
